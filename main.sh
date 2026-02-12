fa2117852cf283745c72674d8ce7ae14f16fa2b3
Burak Yıldırım <burtaza_06@hotmail.com>
2026-02-12 16:11:40 +0300

    feature: Refactor fastlane version check in main.sh
    Update Gemfile handling for fastlane versioning

--- main.sh
+++ main.sh
@@ -13,12 +13,21 @@
       echo "AppStoreConnectApiKey:$AC_API_KEY"
       echo "AppStoreConnectApiKeyFileName:$AC_API_KEY_FILE_NAME"
       echo "appleStoreSubmitApiType:$AC_APPLE_STORE_SUBMIT_API_TYPE"
+      echo "Fastlane Version: $AC_FASTLANE_VERSION"
       
       locale
       curl -o "./$AC_APP_FILE_NAME" -k "$AC_APP_FILE_URL"
 
         bundle init
-        echo "gem \"fastlane\"">>Gemfile
+
+if [ -z "$AC_FASTLANE_VERSION" ] || [ "$AC_FASTLANE_VERSION" = "latest" ]; then
+    echo 'gem "fastlane"' >> Gemfile
+    echo "Using latest fastlane version"
+else
+    echo "Using fastlane version: $AC_FASTLANE_VERSION"
+    echo "gem \"fastlane\", \"$AC_FASTLANE_VERSION\"" >> Gemfile
+fi
+        
         bundle install
         mkdir fastlane
         touch fastlane/Appfile
